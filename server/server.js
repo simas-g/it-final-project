08c4046931f8b61ea585b8fda168d63bc9afbde8
Your Name <federickgrendalo@gmail.com>
2025-10-11 21:51:46 +0300

    first commit

---
+++ server/server.js
@@ -0,0 +1,13 @@
+import express from "express";
+
+const app = express();
+
+const PORT = process.env.PORT || 8080;
+
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
+
+app.get("/", (_req, res) => {
+  res.send("Hello World");
+});
