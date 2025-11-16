727540c74ea5e791e8451d578180b33e7986818c
Simas <federickgrendalo@gmail.com>
2025-11-16 11:42:12 +0200

    odoo configuration

--- server/app.js
+++ server/app.js
@@ -19,11 +19,19 @@ import "dotenv/config";
 const app = express();
 
 const FRONTEND_URL = process.env.FRONTEND_URL || "http://localhost:5173";
+const ODOO_URL = process.env.ODOO_URL || "http://localhost:8069";
 
 app.use(express.json());
 app.use(
   cors({
-    origin: FRONTEND_URL,
+    origin: (origin, callback) => {
+      const allowedOrigins = [FRONTEND_URL, ODOO_URL];
+      if (!origin || allowedOrigins.includes(origin)) {
+        callback(null, true);
+      } else {
+        callback(new Error("Not allowed by CORS"));
+      }
+    },
     methods: ["GET", "POST", "PUT", "DELETE", "PATCH"],
     allowedHeaders: ["Content-Type", "Authorization"],
     credentials: true,
